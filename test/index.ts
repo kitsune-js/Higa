3a9d8effa0925baa9c5d7e27649eb2a2b06dfd20
fantomitechno <fantomitechno@gmail.com>
2022-01-20 18:14:36 +0100

    test: and tests that come with that

--- test/index.ts
+++ test/index.ts
@@ -1,27 +1,45 @@
-import { DiscordClient } from 'higa';
+import { PresenceUpdateStatus } from 'discord-api-types';
+import { Client } from 'higa';
 require("dotenv").config()
 
-const client = new DiscordClient(process.env.DISCORD)
+const client = new Client(
+  process.env.DISCORD,
+  [
+    'GUILD_MESSAGES'
+  ]
+)
 
 client.on('READY', () => {
   console.log("Connected !")
   client.setStatus(
-    "dnd",
-    [
-      {
-        name: 'some tests',
-        type: 3
-      }
-    ]
+    {
+      status: PresenceUpdateStatus.DoNotDisturb,
+      activities: [
+        {
+          name: "some tests",
+          type: 3
+        }
+      ],
+      since: null,
+      afk: false
+    }
   )
 })
 
 client.on('DEBUG', console.log)
 
-client.on("MESSAGE_CREATE", message => {
+client.on("MESSAGE_CREATE", async message => {
   if (message.content.startsWith("!ping")) {
     client.replyToMessage(message.channel_id, message.id, "Pong !")
   } else if (message.content.startsWith("!say")) {
     client.sendMessage(message.channel_id, message.content.replace("!say", ""))
+  } else if (message.content.startsWith("!get")) {
+    console.log(await client.channel.getChannel(message.content.replace("!get ", "")))
+  } else if (message.content.startsWith("!modify")) {
+    console.log(
+      await client.channel.modifyChannel(message.channel_id, {
+        topic: "Ceci est un test qui marche bien (ou pas)" 
+      })
+    )
   }
-})
+}) 
