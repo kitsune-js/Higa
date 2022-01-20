e78ab00ee665eba5b0ca40c9ba13f8bee0db656b
fantomitechno <fantomitechno@gmail.com>
2022-01-20 18:14:24 +0100

    types: typing for all the stuff I've done

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,31 @@
 import { EventEmitter } from 'node:events';
-import { APIChannel, APIMessage } from 'discord-api-types';
+import {
+  APIChannel,
+  APIMessage,
+  GatewayPresenceUpdateData,
+  RESTPatchAPIChannelJSONBody
+} from 'discord-api-types';
 
 declare module 'higa';
+
+const ClientIntents = {
+  GUILDS: 1 << 0,
+  GUILD_MEMBERS: 1 << 1,
+  GUILD_BANS: 1 << 2,
+  GUILD_EMOJIS_AND_STICKERS: 1 << 3,
+  GUILD_INTEGRATIONS: 1 << 4,
+  GUILD_WEBHOOKS: 1 << 5,
+  GUILD_INVITES: 1 << 6,
+  GUILD_VOICE_STATES: 1 << 7,
+  GUILD_PRESENCES: 1 << 8,
+  GUILD_MESSAGES: 1 << 9,
+  GUILD_MESSAGE_REACTIONS: 1 << 10,
+  GUILD_MESSAGE_TYPING: 1 << 11,
+  DIRECT_MESSAGES: 1 << 12,
+  DIRECT_MESSAGE_REACTIONS: 1 << 13,
+  DIRECT_MESSAGE_TYPING: 1 << 14,
+  GUILD_SCHEDULED_EVENTS: 1 << 16
+};
 
 export interface ClientEvents {
   READY: [];
@@ -11,20 +35,34 @@ export interface ClientEvents {
 
 export type Awaitable<T> = T | PromiseLike<T>;
 
-export class DiscordClient extends EventEmitter {
-  constructor(token?: string, debug?: boolean): void;
+class ChannelManager {
+  constructor(token: string, cache: typeof CacheManager): void;
 
-  async setupBebSocket(): void;
+  public async getChannel(id: string): Promise<APIChannel>;
+
+  public async modifyChannel(
+    id: string,
+    options: RESTPatchAPIChannelJSONBody,
+    reason?: string
+  ): Promise<APIChannel>;
+}
+
+export class Client extends EventEmitter {
+  constructor(
+    token?: string,
+    intents: (keyof typeof ClientIntents)[] = []
+  ): void;
+
+  public readonly intents: number;
+
+  public channel: ChannelManager;
 
   on<K extends keyof ClientEvents>(
     event: K,
     listener: (...args: ClientEvents[K]) => Awataible<void>
   ): this;
 
-  public setStatus(
-    status: 'online' | 'dnd' | 'idle' | 'invisible' | 'offline',
-    activities: GatewayActivityUpdateData[]
-  ): void;
+  public setStatus(presence: GatewayPresenceUpdateData): void;
 
   /**
    * Create a DM channel with someone and return it
