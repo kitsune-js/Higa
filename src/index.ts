a4d421a703e8c7c700f274f031171d920c1e222c
fantomitechno <fantomitechno@gmail.com>
2022-01-22 17:24:17 +0100

    doc: start of commenting everything

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export * from './client';
 
+
+/**
+ * 
+ * @param color - A color in Hexadecimal
+ * @returns - Color code for Discord
+ */
 const resolveHexaColor = (color: string) => {
   return parseInt(color.replace('#', ''), 16);
 };
