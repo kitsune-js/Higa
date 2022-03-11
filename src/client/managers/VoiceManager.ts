24ccd4d42d94a30053f917fb2b0bcd2b72b81b04
fantomitechno <fantomitechno@gmail.com>
2022-03-11 22:12:06 +0100

    fix: missreading of the documentation

--- src/client/managers/VoiceManager.ts
+++ src/client/managers/VoiceManager.ts
@@ -1,10 +1,41 @@
 import { RESTGetAPIGuildVoiceRegionsResult } from 'discord-api-types/v9';
 import axios from 'axios';
+import { APIVersion } from '../..';
 
 class VoiceManager {
+  /**
+   * Bot's token
+   */
+  private token: string;
+
+  /**
+   * API Version
+   */
+  public readonly version: APIVersion;
+
+  /**
+   * @param token - Bot's token
+   */
+  constructor(token: string, version: APIVersion) {
+    this.token = token;
+    this.version = version;
+  }
+
+  /**
+   * List all the Voice Regions
+   * @returns - Array of voice regions
+   */
   public async listVoiceRegions(): Promise<RESTGetAPIGuildVoiceRegionsResult> {
     const res = await axios.get<RESTGetAPIGuildVoiceRegionsResult>(
-      `https://discord.com/api/v9/voice/regions`
+      `https://discord.com/api/v9/voice/regions`,
+      {
+        headers: {
+          Authorization: 'Bot ' + this.token,
+          'Content-Type': 'application/json',
+          'User-Agent':
+            'Higa (https://github.com/fantomitechno/Higa, 1.0.0-dev)'
+        }
+      }
     );
     return res.data;
   }
