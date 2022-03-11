85a117cda9275b4ad72de92e4179ede45f1663d1
fantomitechno <fantomitechno@gmail.com>
2022-03-11 22:31:15 +0100

    feat: continuing the user endpoint

--- src/client/managers/UserManager.ts
+++ src/client/managers/UserManager.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import {
+  RESTGetAPICurrentUserGuildsQuery,
+  RESTGetAPICurrentUserGuildsResult,
   RESTGetAPICurrentUserResult,
   RESTGetAPIUserResult,
   RESTPatchAPICurrentUserJSONBody,
   RESTPatchAPICurrentUserResult
 } from 'discord-api-types/v9';
+import { APIVersions } from '../..';
 
 class UserManager {
   /**
@@ -13,15 +16,22 @@ class UserManager {
   private token: string;
 
   /**
-   * @param token - Bot's token
+   * API Version
    */
-  constructor(token: string) {
+  public readonly version: APIVersions;
+
+  /**
+   * @param token - Bot's token
+   * @param version - API Version
+   */
+  constructor(token: string, version: APIVersions) {
     this.token = token;
+    this.version = version;
   }
 
   async getCurrentUser(): Promise<RESTGetAPICurrentUserResult> {
     const res = await axios.get<RESTGetAPICurrentUserResult>(
-      `https://discord.com/api/v9/users/@me`,
+      `https://discord.com/api/v${this.version}/users/@me`,
       {
         headers: {
           Authorization: 'Bot ' + this.token,
@@ -36,7 +46,7 @@ class UserManager {
 
   async getUser(userID: string): Promise<RESTGetAPIUserResult> {
     const res = await axios.get<RESTGetAPIUserResult>(
-      `https://discord.com/api/v9/users/${userID}`,
+      `https://discord.com/api/v${this.version}/users/${userID}`,
       {
         headers: {
           Authorization: 'Bot ' + this.token,
@@ -53,7 +63,7 @@ class UserManager {
     options: RESTPatchAPICurrentUserJSONBody
   ): Promise<RESTPatchAPICurrentUserResult> {
     const res = await axios.patch<RESTPatchAPICurrentUserResult>(
-      `https://discord.com/api/v9/users/@me`,
+      `https://discord.com/api/v${this.version}/users/@me`,
       JSON.stringify(options),
       {
         headers: {
@@ -62,6 +72,24 @@ class UserManager {
           'User-Agent':
             'Higa (https://github.com/fantomitechno/Higa, 1.0.0-dev)'
         }
+      }
+    );
+    return res.data;
+  }
+
+  async getCurrentUserGuilds(
+    options: RESTGetAPICurrentUserGuildsQuery
+  ): Promise<RESTGetAPICurrentUserGuildsResult> {
+    const res = await axios.get<RESTGetAPICurrentUserGuildsResult>(
+      `https://discord.com/api/v${this.version}/users/@me/guilds`,
+      {
+        headers: {
+          Authorization: 'Bot ' + this.token,
+          'Content-Type': 'application/json',
+          'User-Agent':
+            'Higa (https://github.com/fantomitechno/Higa, 1.0.0-dev)'
+        },
+        params: options
       }
     );
     return res.data;
