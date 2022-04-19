fc75c2828c47a0bd05c6e63dd495f2cfd25efe60
fantomitechno <67735304+fantomitechno@users.noreply.github.com>
2022-04-19 17:20:05 +0200

    fix: so it can compile ^^'

--- src/client/managers/CacheManager.ts
+++ src/client/managers/CacheManager.ts
@@ -1,8 +1,9 @@
-import { Channel, Guild, User } from '../../structures';
+import { Guild, User } from '../../structures';
 import { Role } from '../../structures/Role';
+import { APIChannel } from 'discord-api-types/v9';
 
 class CacheManager {
-  public channels = new Map<string, Channel>();
+  public channels = new Map<string, APIChannel>();
   public roles = new Map<string, Role>();
   public guilds = new Map<string, Guild>();
   public users = new Map<string, User>();
