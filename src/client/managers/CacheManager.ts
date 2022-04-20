4f0e537c249ea212709abee89be053b163f2d15b
fantomitechno <67735304+fantomitechno@users.noreply.github.com>
2022-04-20 17:20:34 +0200

    fix: ik what i need to be cached

--- src/client/managers/CacheManager.ts
+++ src/client/managers/CacheManager.ts
@@ -1,11 +1,25 @@
-import { Channel, Guild, User } from '../../structures';
-import { Role } from '../../structures/Role';
+import {
+  Channel,
+  Guild,
+  GuildMember,
+  GuildScheduledEvent,
+  Message,
+  Role
+} from '../../structures';
 
 class CacheManager {
   public channels = new Map<string, Channel>();
   public roles = new Map<string, Role>();
   public guilds = new Map<string, Guild>();
-  public users = new Map<string, User>();
+  /**
+   * @warning The Guild Member in that cache doesn't have the permissions property cached
+   */
+  public guildMembers = new Map<
+    [string, string],
+    GuildMember & { guild_id: string }
+  >();
+  public guildScheduledEvents = new Map<string, GuildScheduledEvent>();
+  public messages = new Map<string, Message>();
 }
 
 export { CacheManager };
