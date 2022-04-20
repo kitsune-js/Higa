943fde8a47d1f52eb0031b1e25e6a7ade84f220f
fantomitechno <67735304+fantomitechno@users.noreply.github.com>
2022-04-20 13:24:42 +0200

    feat: Interaction typing

--- src/structures/Interaction.ts
+++ src/structures/Interaction.ts
@@ -1,5 +1,6 @@
 import { LocalizedString } from '.';
 import { ChannelType, Message } from './Channel';
+import { Emoji } from './Emoji';
 import { GuildMember } from './Guild';
 import { User } from './User';
 
@@ -84,6 +85,75 @@ enum ApplicationCommandPermissionType {
   USER = 2
 }
 
+interface Component {
+  type: ComponentType;
+}
+
+enum ComponentType {
+  ACTION_ROW = 1,
+  BUTTON = 2,
+  SELECT_MENU = 3,
+  TEXT_INPUT = 4
+}
+
+interface ActionRow extends Component {
+  type: ComponentType.ACTION_ROW;
+  components: Component[];
+}
+
+interface Button extends Component {
+  type: ComponentType.BUTTON;
+  style: ButtonStyle;
+  label?: string;
+  emoji?: Emoji;
+  custom_id?: string;
+  url?: string;
+  disabled?: boolean;
+}
+
+enum ButtonStyle {
+  PRIMARY = 1,
+  SECONDARY = 2,
+  SUCCESS = 3,
+  DANGER = 4,
+  LINK = 5
+}
+
+interface SelectMenu extends Component {
+  type: ComponentType.SELECT_MENU;
+  custom_id: string;
+  options: SelectMenuOption[];
+  placeholder?: string;
+  min_values?: number;
+  max_values?: number;
+  disabled?: boolean;
+}
+
+interface SelectMenuOption {
+  label: string;
+  value: string;
+  description?: string;
+  emoji?: Emoji;
+  default?: boolean;
+}
+
+interface TextInput extends Component {
+  type: ComponentType.TEXT_INPUT;
+  custom_id: string;
+  style: TextInputStyle;
+  label: string;
+  min_length?: number;
+  max_length?: number;
+  required?: boolean;
+  value?: string;
+  placeholder?: string;
+}
+
+enum TextInputStyle {
+  SHORT = 1,
+  PARAGRAPH = 2
+}
+
 interface Interaction {
   id: string;
   application_id: string;
@@ -138,3 +208,22 @@ interface MessageInteraction {
   user: User;
   member: GuildMember;
 }
+
+export {
+  ApplicationCommand,
+  ApplicationCommandOption,
+  ApplicationCommandOptionChoice,
+  ApplicationCommandInteractionDataOption,
+  GuildApplicationCommandPermissions,
+  ApplicationCommandPermissionType,
+  Component,
+  ActionRow,
+  Button,
+  SelectMenu,
+  TextInput,
+  Interaction,
+  InteractionType,
+  InteractionData,
+  ResolvedData,
+  MessageInteraction
+};
